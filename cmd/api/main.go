934f8fb13be6994675a679bec940a5851c57103d
Saident <averilprimayuda@gmail.com>
2026-04-13 16:58:12 +0700

    update

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -41,7 +41,7 @@ type SaveBouquetRequest struct {
 func enableCORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		if r.Method == "OPTIONS" {
--- internal/game/daily.go
+++ internal/game/daily.go
@@ -6,8 +6,8 @@ import (
 )
 
 var MasterFlowerPool = []string{
-	"rose_red.png", "rose_white.png", "tulip_yellow.png", 
-	"daisy_basic.png", "lily_white.png", "sunflower.png", 
+	"rose_red.png", "rose_white.png", "tulip_yellow.png",
+	"daisy_basic.png", "lily_white.png", "sunflower.png",
 	"orchid_purple.png", "peony_pink.png", "cherry_blossom.png",
 	"blue_hydrangea.png",
 }
@@ -15,15 +15,13 @@ var MasterFlowerPool = []string{
 func GetDailyInventory(hasSpecialUnlock bool) []string {
 	loc := time.FixedZone("UTC+7", 7*60*60)
 	now := time.Now().In(loc)
-	// Using UTC ensures it changes at the same moment everywhere, 
-	// but you can change this to a specific timezone if you want it to reset at her local midnight.
 	seedDate := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, loc)
-	
+
 	r := rand.New(rand.NewSource(seedDate.Unix()))
 
 	poolCopy := make([]string, len(MasterFlowerPool))
 	copy(poolCopy, MasterFlowerPool)
-	
+
 	r.Shuffle(len(poolCopy), func(i, j int) {
 		poolCopy[i], poolCopy[j] = poolCopy[j], poolCopy[i]
 	})
@@ -31,7 +29,7 @@ func GetDailyInventory(hasSpecialUnlock bool) []string {
 	dailySelection := poolCopy[:5]
 
 	if hasSpecialUnlock {
-		dailySelection[0] = "rare_glowing_lotus.gif" 
+		dailySelection[0] = "rare_glowing_lotus.gif"
 	}
 
 	return dailySelection
